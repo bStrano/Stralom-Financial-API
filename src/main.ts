e86e84e8667164e8ba731afac512a0e4fe8d06cd
Bruno Strano <bruno_strano@hotmail.com>
2023-08-10 21:59:59 -0300

    🔧 Cors config

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,17 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import { initializeSwagger } from './config/swagger.config';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  const configurationService = app.get(ConfigService);
+  if (configurationService.get('NODE_ENV') !== 'local') {
+    app.enableCors({
+      origin: [/.*\.stralom\.com$/],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    });
+  }
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
   app.setGlobalPrefix('/api/financial');
   initializeSwagger(app);
